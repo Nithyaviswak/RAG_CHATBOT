e1828630e4252ffabdc6af47c16c7e95a0c0c321
Nithyaviswak <nviswaks@gmail.com>
2026-03-01 16:36:43 +0530

    changes in entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,4 @@
 
-API_URL=${API_URL:-http://localhost:5000/api}
+API_URL=${API_URL:-https://rag-chatbot-53e0.onrender.com/}
 sed -i "s|window.API_BASE || window.ENV_API_URL || 'https://rag-chatbot-53e0.onrender.com/'|'${API_URL}'|g" /usr/share/nginx/html/index.html
 exec "$@"
